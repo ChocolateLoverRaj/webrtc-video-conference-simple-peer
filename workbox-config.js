5db618e99f64b942d6104608908cacad5b09c98d
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-08-02 10:41:17 -0700

    Pwa with workbox

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'public/',
+	globPatterns: [
+		'**/*.{html,json}'
+	],
+	swDest: 'public/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
